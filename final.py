9f6ff503708ce4aa966e291935713ef29f5e6814
miguelflores-0906 <miggy_4th@yahoo.com>
2022-12-08 00:34:33 +0800

    time.sleep(15)     while True:         personnel_id = buffer.get() #queue.get()         if personnel_id == None:             break         # wd = webdriver.Edge(URL + '?personnel=' + personnel_id)         wd = webdriver.Edge(PATH)         wd.get(f'{URL}?personnel={personnel_id}')
            name = wd.find_element(By.TAG_NAME, 'h3').get_attribute('innerHTML')
    
            dept_info = wd.find_element(By.CLASS_NAME, 'list-unstyled.text-capitalize.text-center').find_elements(By.TAG_NAME,'li')
            position = dept_info[0].get_attribute('innerHTML')[6:-7]
            department = dept_info[1].get_attribute('innerHTML')[6:-7]
    
            email_button = wd.find_element(By.CSS_SELECTOR, '.btn.btn-sm.btn-block.text-capitalize')
            email = email_button.get_attribute('href')[7:]
    
            print([name, department, position, email])
            wd.close()

--- final.py
+++ final.py
@@ -27,21 +27,94 @@ PATH = "D:\Documents\DLSU\Year 4 Term 1\OPESY\email_scraper\parallel_programming
 # Constants
 URL = "https://www.dlsu.edu.ph/staff-directory"
 
+
+class producer(multiprocessing.Process):
+    def __init__(self, buffer, thread_id = 1):
+        multiprocessing.Process.__init__(self)
+        self.queue = buffer
+        self.thread_id = thread_id
+    def run(self):
+        print("I will do the producer task")
+        # producer_task(self.queue)
+        wd = webdriver.Edge(PATH)
+        wd.get(URL)
+
+        time.sleep(10)
+
+        while True:
+
+            scroll(wd, 2)
+            scroll(wd, 2)
+
+            # find button with onClick = "personnelLoadMoreAction()"
+            try:
+                button = wd.find_element(By.XPATH, "//button[@onclick='personnelLoadMoreAction()']")
+                print(button)
+                person = wd.find_elements(By.NAME, "personnel")
+                print(len(person))
+
+                for p in person:
+                    if p.get_attribute('value') not in self.queue:
+                        self.queue.put(p.get_attribute('value'))
+
+                button.click()
+                time.sleep(10)
+            except:
+                print("cannot find button")
+                break
+        self.queue.put(None)
+        self.queue.put(None)
+        self.queue.put(None)
+        self.queue.put(None)
+        self.queue.put(None)
+        # get the value attribute of each personnel
+
+        wd.close()
+
+class consumer(multiprocessing.Process):
+    def __init__(self, buffer, thread_id = 1):
+        multiprocessing.Process.__init__(self)
+        self.queue = buffer
+        self.thread_id = thread_id
+    def run(self):
+        print("Doing the consumer task")
+        time.sleep(15)
+        while True:
+            personnel_id = self.queue.get() #queue.get()
+            if personnel_id == None:
+                break
+            # wd = webdriver.Edge(URL + '?personnel=' + personnel_id)
+            wd = webdriver.Edge(PATH)
+            wd.get(f'{URL}?personnel={personnel_id}')
+
+
+            name = wd.find_element(By.TAG_NAME, 'h3').get_attribute('innerHTML')
+
+            dept_info = wd.find_element(By.CLASS_NAME, 'list-unstyled.text-capitalize.text-center').find_elements(By.TAG_NAME,'li')
+            position = dept_info[0].get_attribute('innerHTML')[6:-7]
+            department = dept_info[1].get_attribute('innerHTML')[6:-7]
+
+            email_button = wd.find_element(By.CSS_SELECTOR, '.btn.btn-sm.btn-block.text-capitalize')
+            email = email_button.get_attribute('href')[7:]
+
+            print([name, department, position, email])
+            wd.close()
+
 def scroll(wd, delay):
     wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(delay)
 
-def producer(buffer):
+def producer_task(buffer):
 
     wd = webdriver.Edge(PATH)
     wd.get(URL)
 
-    time.sleep(1)
+    time.sleep(10)
 
     while True:
 
-        scroll(wd, 5)
-        scroll(wd, 5)
+        scroll(wd, 2)
+        scroll(wd, 2)
 
         # find button with onClick = "personnelLoadMoreAction()"
         try:
@@ -53,21 +126,28 @@ def producer(buffer):
             for p in person:
                 if p.get_attribute('value') not in buffer:
                     buffer.put(p.get_attribute('value'))
-                    
+
             button.click()
             time.sleep(10)
         except:
             print("cannot find button")
             break
-
+    buffer.put(None)
+    buffer.put(None)
+    buffer.put(None)
+    buffer.put(None)
+    buffer.put(None)
     # get the value attribute of each personnel
 
     wd.close()
 
-def consumer(buffer):
+def consumer_task(buffer):
     # wd = webdriver.Chrome(service = SERVICE, options = OPTIONS)
+    time.sleep(15)
     while True:
         personnel_id = buffer.get() #queue.get()
+        if personnel_id == None:
+            break
         # wd = webdriver.Edge(URL + '?personnel=' + personnel_id)
         wd = webdriver.Edge(PATH)
         wd.get(f'{URL}?personnel={personnel_id}')
@@ -85,23 +165,22 @@ def consumer(buffer):
         print([name, department, position, email])
         wd.close()
     
-lock = multiprocessing.Lock()
+# lock = multiprocessing.Lock()
 
-buffer = queue.Queue()
-
-wd = webdriver.Edge(PATH)
-wd.get(URL)
+buffer = multiprocessing.Queue()
 
 num_consumers = 4
 
-producer = Thread(target = producer, args = (buffer,))
-producer.start()
+p = producer(buffer, 1)
+print(p)
+p.start()
 
 consumers = []
 for i in range(num_consumers):
-    consumers.append(Thread(target = consumer, args = (buffer,)))
-    consumers[i].start()
+    c = consumer(buffer, i)
+    consumers.append(c)
+    c.start()
 
-producer.join()
-for i in range(num_consumers):
-    consumers[i].join()
+p.join()
+for c in consumers:
+    c.join()
